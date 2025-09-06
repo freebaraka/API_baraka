e9ca713ce0598516899db8c1153cd9cc39a0ebc2
freebaraka <baraka.onserio@strathmore.edu>
2025-09-06 14:55:57 +0300

    created log in & out

--- Forms/forms.php
+++ Forms/forms.php
@@ -1,5 +1,8 @@
 <?php
 class forms{
+    private function submit_button($value){
+        echo "<input type='submit' value ='{$value}'>";
+    }
     public function signup(){
         ?><h2>Signup Form BBIT</h2>
         <form action="submit_signup.php" method="post">
@@ -10,6 +13,20 @@ class forms{
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required><br><br>
             <input type="submit" value="Sign Up">
+            <?php $this-> submit_button('Sign Up'); ?>
+        </form>
+        <?php    
+    }
+
+    public function login(){
+        ?>
+        <h2>Login Form</h2>
+        <form action="submit_login.php" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            <?php $this->submit_button('Log In'); ?> <a href="index.php">Don't have an account? Sign Up</a>;
         </form>
         <?php    
     }
