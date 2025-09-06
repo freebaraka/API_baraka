7a0a522e69e4936d1eab5e9508ef3910176e4ffa
freebaraka <baraka.onserio@strathmore.edu>
2025-09-06 13:05:28 +0300

    init code

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+class MyClass {
+    public function heading() {
+        echo "Welcome to BBIT DevOps!";
+    }
+    public function myMethod(){
+        echo "<p>This is a new semester.</p>";
+    }
+
+    public function footer() {
+        echo "<footer>Contact us at <a href='mailto:info@bbit.edu'>info@bbit.edu</a></footer>";
+    }
+}
+
+//create instance of MyClass
+$instance = new MyClass();
+
+//calling the methods
+$instance->myMethod();
+$instance->heading();
+$instance->footer();
